4d8bc8e248b23d18509c1072423f7eb314978468
Georgee <olisaevgs@gmail.com>
2014-08-05 16:14:44 +0400

    add half of putEvent request

--- library/src/main/java/olis/getsyproximity/library/Request/BasicRequest.java
+++ library/src/main/java/olis/getsyproximity/library/Request/BasicRequest.java
@@ -1,5 +1,8 @@
 package olis.getsyproximity.library.Request;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by GTO on 02.08.2014.
  */
@@ -15,18 +18,107 @@ public class BasicRequest {
     protected String gender;
     protected String birthday;
 
+    protected ArrayList<Event> eventsList = new ArrayList<Event>();
+
+    protected class Event {
+        private int timestamp;
+        private String type;
+        private Data data;
+        private Description description;
+        protected Event (int timestamp, String type, Data data, Description description)
+
+        protected int getTimestamp() {
+            return timestamp;
+        }
+
+        protected String getType() {
+            return type;
+        }
+
+        protected void setTimestamp(int timestamp) {
+            this.timestamp = timestamp;
+        }
+
+        protected void setType(String tupe) {
+            this.type = tupe;
+        }
+    }
+
+    protected class Description {
+        private String notified;
+        private String swiped;
+        private String displayed;
+        private String entered;
+        private String exited;
+        protected Description (String notified, String swiped, String displayed) {
+            this.notified = notified;
+            this.swiped = swiped;
+            this.displayed = displayed;
+        }
+        protected Description (String entered, String exited) {
+            this.entered = entered;
+            this.exited = exited;
+        }
+        protected class BeaconDescription extends Description {
+            protected BeaconDescription(String entered, String exited) {
+                super(entered, exited);
+            }
+        }
+        protected class ExperienceDescription extends Description{
+            protected ExperienceDescription(String notified, String swiped, String displayed) {
+                super(notified, swiped, displayed);
+            }
+        }
+    }
+
+    protected class Data {
+        private int id;
+        private int uuid;
+        private int major;
+        private int minor;
+        private int rssi;
+        private String distance;
+
+        protected Data (int id) {
+            this.id = id;
+        }
+
+        protected Data (int uuid, int major, int minor, int rssi, String distance) {
+            this.uuid = uuid;
+            this.major = major;
+            this.minor = minor;
+            this.rssi = rssi;
+            this.distance = distance;
+        }
+
+        protected class BeaconData extends Data {
+            protected BeaconData(int uuid, int major, int minor, int rssi, String distance) {
+                super(uuid, major, minor, rssi, distance);
+            }
+        }
+
+        protected class ExperienceData extends Data {
+            protected ExperienceData(int id) {
+                super(id);
+            }
+        }
+    }
+
     protected BasicRequest(){
     }
+
     protected BasicRequest(String userId, String loginType) {
         this.userId = userId;
         this.loginType = loginType;
     }
+
     protected BasicRequest(String userId, String loginType, int appId, String appToken){
         this.userId = userId;
         this.loginType = loginType;
         this.appId = appId;
         this.appToken = appToken;
     }
+
     protected BasicRequest(String userId, String loginType, int appId, String appToken, String firstName, String lastName, String gender, String birthday) {
         this.userId = userId;
         this.loginType = loginType;
